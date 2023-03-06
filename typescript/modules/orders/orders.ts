1cc9447d4209e58202fc1caa9e0950df5d148c6d
Geoff <geoff.petrides@gmail.com>
2023-03-06 11:54:45 +0000

    Updated: changed when order update logs last update

--- typescript/modules/orders/orders.ts
+++ typescript/modules/orders/orders.ts
@@ -167,11 +167,11 @@ export const linnGet = async () => {
 
     let data = qResult.Results
     console.log("New orders found: " + data.length)
+    let currentDate = new Date()
+    await setData("Server", {id: "Orders"}, {lastUpdate: toSqlDate < currentDate ? toSqlDate : currentDate})
+
     if (data.length > 0) await processOrders(data)
 
-    let currentDate = new Date()
-
-    await setData("Server", {id: "Orders"}, {lastUpdate: toSqlDate < currentDate ? toSqlDate : currentDate})
     return
 }
 
